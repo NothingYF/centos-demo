39fd4846f0ec9e26b681caaf2c944999978715f1
NothingYF <84727906@qq.com>
2018-10-15 02:48:41 +0000

    commit

--- run.sh
+++ run.sh
@@ -1,12 +1,20 @@
 #!/bin/sh
 
 REPO=nothingdocker
-IMAGE_NAME=`basename $PWD`
+IMAGE_NAME=centos-demo
+TAG=mongodb-redis-rmq
 DOCKER_NAME=${1:-demo}
 
+docker stop $DOCKER_NAME
 docker rm -f $DOCKER_NAME
 docker run -d --restart=always --privileged \
 	--name $DOCKER_NAME \
 	-v /sys/fs/cgroup:/sys/fs/cgroup:ro \
-	$REPO/$IMAGE_NAME
+	-v /data/server:/data/server \
+        -v /data/config:/data/config \
+        -v /data/config/nginx:/etc/nginx \
+        -v /data/config/supervisor:/etc/supervisord.d \
+        -v /data/mongo/db:/var/lib/mongo \
+        -v /data/mongo/logs:/var/log/mongodb \
+	$REPO/$IMAGE_NAME:$TAG
 docker exec -it $DOCKER_NAME bash
